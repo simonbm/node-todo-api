fd6e9676561f5acce5799a31de14f05ce6fb6bff
Simon Boyce-Maynard <simon.boyce-maynard@mobileforming.com>
2018-03-19 17:53:24 -0700

    Added test cases for service

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,12 +2,23 @@
 
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    _id: new ObjectID(), 
+    text: 'First test todo'
+}, {
+    _id: new ObjectID(), 
+    text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -27,10 +38,54 @@ describe('POST /todos', () => {
             }
 
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(1);
-                expect(todos[0].text).toBe(text);
+                expect(todos.length).toBe(3);
+                expect(todos[2].text).toBe(text);
                 done();
             }).catch((e) => done(e));
         });
     });
 });
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => { 
+            expect(res.body.todo.text).toBe(todos[0].text);
+        }).end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var newId = new ObjectID();
+
+        request(app)
+        .get(`/todos/${newId.toHexString()}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+
+        var invalidID = 'dhdhdhdhdhdhd';
+        request(app)
+        .get(`/todos/${invalidID}`)
+        .expect(404)
+        .end(done);
+        
+    });
+});
+
+
