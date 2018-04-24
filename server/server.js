1229b66bb82335149893f5a75f7209057fbb5a92
Simon Boyce-Maynard <sboycemaynard@Simons-MBP-2018.local>
2018-04-24 16:14:52 -0700

    Fixed Delete Bug

--- server/server.js
+++ server/server.js
@@ -151,7 +151,7 @@ app.post('/users/login', (req, res) => {
 
 app.delete('/users/me/token', authenticate, (req, res) => {
     req.user.removeToken(req.token).then((user) => {
-        res.status(200).send(user);
+        res.status(200).send();
     }, () => {
         res.status(400).send();
     });
